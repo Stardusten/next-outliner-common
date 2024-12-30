5d2478f63f469547a14af234fcd48dad72564f62
Stardust <xiangbaobao2003@gmail.com>
2024-12-30 16:47:52 +0800

    update

---
+++ api-call/fs.ts
@@ -0,0 +1,85 @@
+import { AxiosInstance } from "axios";
+import { usePostApi } from "../helper-functions/usePostApi";
+import {
+  FsEnsureAttachmentsDirSchema,
+  FsGetAttachmentSignedUrlSchema,
+  FsLsSchema,
+  FsStatSchema,
+  FsUploadSchema,
+} from "../type-and-schemas/api/fs";
+import { RESP_CODES } from "../constants";
+import { RespSchema } from "../type-and-schemas/resp";
+import { z } from "zod";
+
+export const fsLs = usePostApi(`/fs/ls`, FsLsSchema.request, FsLsSchema.result);
+
+export const fsStat = usePostApi(
+  `/fs/stat`,
+  FsStatSchema.request,
+  FsStatSchema.result,
+);
+
+/**
+ * @param files [targetPath, File][]
+ */
+export const fsUpload = async (
+  files: [string, File][],
+  config?: z.infer<typeof FsUploadSchema.request>,
+) => {
+  const endpoint = `/fs/upload`;
+  try {
+    // 要使用这个函数，必须将 () => AxiosInstance 挂载到 globalThis 上
+    let axios: AxiosInstance;
+    try {
+      axios = (globalThis as any).getAxios();
+    } catch (error) {
+      console.error(`[NO_AXIOS] ${error}`);
+      return { success: false, code: RESP_CODES.NO_AXIOS };
+    }
+
+    const formData = new FormData();
+    files.forEach((file) => {
+      formData.append("targetPath", file[0]);
+      formData.append("file", file[1]);
+    });
+
+    const res = await axios.post(endpoint, formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+      params: config,
+    });
+
+    if (!res) {
+      console.error(`[EMPTY_RESPONSE] endpoint=${endpoint}`);
+      return { success: false, code: RESP_CODES.UNKNOWN_ERROR };
+    }
+
+    const resSchema = RespSchema(FsUploadSchema.result);
+    const result = resSchema.safeParse(res.data);
+
+    if (!result.success) {
+      console.error(
+        `[INVALID_RESPONSE] endpoint=${endpoint}}, response=${JSON.stringify(res.data)}, validationErrors=${JSON.stringify(result.error.errors)}`,
+      );
+      return { success: false, code: RESP_CODES.INVALID_RESPONSE };
+    }
+
+    return { success: true, code: RESP_CODES.SUCCESS };
+  } catch (error) {
+    console.error(`[UNKNOWN_ERROR] endpoint=${endpoint}}, error=${error}`);
+    return { success: false, code: RESP_CODES.UNKNOWN_ERROR };
+  }
+};
+
+export const fsEnsureAttachmentsDir = usePostApi(
+  `/fs/ensure-attachments-dir`,
+  FsEnsureAttachmentsDirSchema.request,
+  FsEnsureAttachmentsDirSchema.result,
+);
+
+export const fsGetAttachmentSignedUrl = usePostApi(
+  `/fs/get-attachment-signed-url`,
+  FsGetAttachmentSignedUrlSchema.request,
+  FsGetAttachmentSignedUrlSchema.result,
+);
---
+++ type-and-schemas/api/fs.ts
@@ -0,0 +1,49 @@
+import { z } from "zod";
+import { DirentsSchema } from "../../../utils/type-and-schemas/fs";
+
+export const FsLsSchema = {
+  request: z.object({
+    basePath: z.string(),
+    includeHidden: z.boolean().optional(),
+    recursive: z.boolean().optional(),
+    maxDepth: z.number().optional(),
+  }),
+  result: DirentsSchema,
+};
+
+export const FsStatSchema = {
+  request: z.object({
+    path: z.string(),
+  }),
+  result: z.object({
+    ctime: z.coerce.date(),
+    mtime: z.coerce.date(),
+    size: z.number(),
+  }),
+};
+
+export const FsUploadSchema = {
+  request: z.object({
+    // 是否覆盖已存在的文件
+    overwrite: z.boolean().optional(),
+    // 是否创建不存在的目录
+    mkdir: z.boolean().optional(),
+  }),
+  result: z.undefined(),
+};
+
+export const FsEnsureAttachmentsDirSchema = {
+  request: z.object({}),
+  result: z.any(),
+};
+
+export const FsGetAttachmentSignedUrlSchema = {
+  request: z.object({
+    path: z.string(),
+    attachment: z.boolean().optional(),
+    inferMimeType: z.boolean().optional(),
+  }),
+  result: z.object({
+    signedUrl: z.string(),
+  }),
+};
