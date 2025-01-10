e07511a537d2e1376ea48e79ed936f071ad0190e
Stardust <xiangbaobao2003@gmail.com>
2025-01-10 18:59:57 +0800

    update

--- helper-functions/block.ts
+++ helper-functions/block.ts
@@ -1,11 +1,11 @@
+import type { MinimalBlock } from "@/context/blocks/view-layer/blocksManager";
 import type { BlockStatus } from "../type-and-schemas/block/block-status";
 
 export const extractBlockStatus = (blockStatus: BlockStatus) => {
   const type = blockStatus & 0b11;
   const fold = blockStatus & 0b100;
   return {
-    type:
-      type == 0 ? "normalBlock" : type == 1 ? "mirrorBlock" : "virtualBlock",
+    type: type == 0 ? "normalBlock" : type == 1 ? "mirrorBlock" : "virtualBlock",
     fold: fold == 0 ? false : true,
   } as const;
 };
@@ -14,8 +14,5 @@ export const calcBlockStatus = (
   type: "normalBlock" | "mirrorBlock" | "virtualBlock",
   fold: boolean,
 ) => {
-  return (
-    (type == "normalBlock" ? 0 : type == "mirrorBlock" ? 1 : 2) |
-    (fold ? 0b100 : 0)
-  );
+  return (type == "normalBlock" ? 0 : type == "mirrorBlock" ? 1 : 2) | (fold ? 0b100 : 0);
 };
