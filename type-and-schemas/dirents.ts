a676add2604961a1968001536a733f51321dd526
Stardust <xiangbaobao2003@gmail.com>
2025-01-13 22:07:12 +0800

    update

--- type-and-schemas/dirents.ts
+++ type-and-schemas/dirents.ts
@@ -2,8 +2,21 @@ import { z } from "zod";
 
 export type Dirents = Record<
   string,
-  | { isDirectory: true; name: string; subDirents: Dirents }
-  | { isDirectory: false; name: string }
+  | {
+      isDirectory: true;
+      name: string;
+      ctime: Date;
+      mtime: Date;
+      size: number;
+      subDirents: Dirents;
+    }
+  | {
+      isDirectory: false;
+      name: string;
+      ctime: Date;
+      mtime: Date;
+      size: number;
+    }
 >;
 
 export const DirentsSchema = z.record(
@@ -12,11 +25,17 @@ export const DirentsSchema = z.record(
     z.object({
       isDirectory: z.literal(true),
       name: z.string(),
+      ctime: z.coerce.date(),
+      mtime: z.coerce.date(),
+      size: z.number(),
       subDirents: z.lazy(() => DirentsSchema),
     }),
     z.object({
       isDirectory: z.literal(false),
       name: z.string(),
+      ctime: z.coerce.date(),
+      mtime: z.coerce.date(),
+      size: z.number(),
     }),
   ]),
 ) as z.ZodType<Dirents>;
